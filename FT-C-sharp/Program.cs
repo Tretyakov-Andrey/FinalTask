99644e83fd8bc13bf6b20a4c771a8165ce677feb
Andrey <this.is.ya8@gmail.com>
2022-09-20 13:15:53 +1000

    finalnoe oformlenie

--- FT-C-sharp/Program.cs
+++ FT-C-sharp/Program.cs
@@ -3,32 +3,32 @@
 //  выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись
 //  исключительно массивами.
 
-string[] GetNumbers (int size)
+string[] GetNumbers(int size)
 {
     string[] numbers = new string[size];
     for (int i = 0; i < size; i++)
     {
-        numbers [i] =  Convert.ToString(new Random().Next (1,10000));
-        System.Console.Write($"{numbers[i], 8}");
+        numbers[i] = Convert.ToString(new Random().Next(1, 10000));
+        System.Console.Write($"{numbers[i],8}");
     }
     return numbers;
 }
 
-string[] Result (string[] numbers)
+string[] Result(string[] numbers)
 {
     System.Console.WriteLine();
     int count = 0;
     for (int j = 0; j < numbers.Length; j++)
     {
         count = 0;
-        
+
         for (int n = 0; n < numbers[j].Length; n++)
         {
             count++;
         }
-        if (count < 4) System.Console.Write($"{numbers[j], 8}");
+        if (count < 4) System.Console.Write($"{numbers[j],8}");
     }
     return numbers;
 }
 
-Result (GetNumbers (10)); 
+Result(GetNumbers(10));
